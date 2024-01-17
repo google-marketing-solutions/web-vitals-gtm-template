c23c745f43352f048fb81b4dcf177c61f57e90eb
Adam Read <adamread@google.com>
2024-01-17 09:54:11 +0000

    Adding code to create the template and deploy a new tag.
    The template is created using the .tpl file provided by the template
    solution. The file needs to be manually chosen by the user to allow JavaScript to
    read the contents.
    
    The tag is configured using the web page and the deployed using that configuration.

--- automation/gtm_automation.js
+++ automation/gtm_automation.js
@@ -37,17 +37,178 @@ function authorizeApp(event) {
   });
   gapi.load('client', () => {
     gapi.client.init({})
-      .then(function () {
+      .then(function() {
         gapi.client.load('https://tagmanager.googleapis.com/$discovery/rest?version=v2');
       })
-      .then(deployTemplate());
+      .then(runDeployment());
   });
 }
 
+/**
+ * Start point for deploying the template to GTM. The template file is read in
+ * here and then passed to createTemplate() to start the process.
+ */
+function runDeployment() {
+  const gtmURL = document.getElementById('gtm-url').value;
+  const parent = gtmURL.substring(gtmURL.indexOf('accounts'));
+
+  const templateFile = document.getElementById('template-file').files[0];
+  const templateReader = new FileReader();
+  templateReader.addEventListener('load', () => {
+    createTemplate(parent, templateReader.result)
+  });
+  templateReader.readAsText(templateFile);
+}
 
 /**
- * Stub for adding the temaplate to the workspace.
+ * Creates the template used to measure the Core web Vital Metrics in the
+ * provided workspace.
+ *
+ * If the template is successfully created, the template ID is passed to
+ * createTag() to continue the deployment.
+ *
+ * @param parent {Map<string, string>} The GTM parent being deployed to.
+ * @param content {string} The content of the template.
  */
-function deployTemplate() {
+function createTemplate(parent, content) {
+  tokenClient.callback = (resp) => {
+    if (resp.error !== undefined) {
+      throw (resp);
+    }
+    gapi.client.tagmanager.accounts.containers.workspaces.templates.create({
+      parent: parent,
+      templateData: content,
+    })
+      .then((gtmResp) => {
+        const templateID = gtmResp.result.templateId;
+        createTag(parent, templateID);
+      })
+      .catch((err) => {
+        console.error('Error creating template: ' + err);
+      });
 
+  };
+  if (gapi.client.getToken() == null) {
+    tokenClient.requestAccessToken({ prompt: 'consent' });
+  } else {
+    tokenClient.requestAccessToken({ prompt: '' });
+  }
 }
+
+/**
+ * Creates a tag using the previously installed template. The template config
+ * is taken from the webpage.
+ *
+ * @param parent {string} The GTM parent string to create the tag in.
+ * @param templateID {string} The ID the template was assigned when it was
+ *     created.
+ */
+function createTag(parent, templateID) {
+  const containerID = parent.split('/')[3];
+
+  const tag = {
+    parent: parent,
+    resource: {
+      name: 'Web Vitals Tag',
+      type: 'cvt_' + containerID + '_' + templateID,
+      firingTriggerId: [
+        "2147479553" // Trigger ID for standard "All Pages" trigger
+      ],
+      tagFiringOption: "oncePerEvent",
+      parameter: [
+        {
+          type: 'template',
+          key: 'libraryConfig',
+          value: 'unpkg',
+        },
+        {
+          type: 'template',
+          key: 'build',
+          value: document.getElementById('use-attribution').checked ?
+              'attribution' : 'standard',
+        },
+        {
+          type: 'template',
+          key: 'eventName',
+          value: 'webVitals',
+        },
+        {
+          type: 'template',
+          key: 'metrics',
+          value: 'specificMetrics',
+        },
+        {
+          type: 'boolean',
+          key: 'ttfb',
+          value: document.getElementById('cwv-TTFB').checked.toString()
+        },
+        {
+          type: 'boolean',
+          key: 'fcp',
+          value: document.getElementById('cwv-FCP').checked.toString()
+        },
+        {
+          type: 'boolean',
+          key: 'lcp',
+          value: document.getElementById('cwv-LCP').checked.toString()
+        },
+        {
+          type: 'boolean',
+          key: 'cls',
+          value: document.getElementById('cwv-CLS').checked.toString()
+        },
+        {
+          type: 'boolean',
+          key: 'fid',
+          value: document.getElementById('cwv-FID').checked.toString()
+        },
+        {
+          type: 'boolean',
+          key: 'inp',
+          value: document.getElementById('cwv-INP').checked.toString()
+        },
+      ],
+    }
+  };
+  if (document.getElementById('use-unpkg').checked) {
+    tag.resource.parameter.push({
+      type: 'template',
+      key: 'libraryConfig',
+      value: 'unpkg',
+    });
+  } else {
+    tag.resource.parameter.push({
+      type: 'template',
+      key: 'customURL',
+      value: document.getElementById('library-url').value,
+    });
+  }
+  if (document.getElementById('event-name').value === 'webVitals') {
+    tag.resource.parameter.push({
+      type: 'template',
+      key: 'eventName',
+      value: 'webVitals',
+    });
+  } else {
+    tag.resource.parameter.push({
+      type: 'template',
+      key: 'eventName',
+      value: 'custom',
+    });
+    tag.resource.parameter.push({
+      type: 'template',
+      key: 'customEventName',
+      value: document.getElementById('event-name').value,
+    });
+  }
+
+  gapi.client.tagmanager.accounts.containers.workspaces.tags.create(tag)
+      .then((gtmResp) => {
+        console.log("Created the Tag!");
+      })
+    .catch((err) => {
+      console.error('Error creating template: ' + err.result.error.details[0].detail);
+    });
+}
+
+document.getElementById('deploy-form').addEventListener('submit', authorizeApp);
